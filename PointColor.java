acd8eada789c4416b8ada07eb16fa43d9b576815
TankNee <nitongke@gmail.com>
2018-10-18 20:52:27 +0800

    hiahiahia

---
+++ PointColor.java
@@ -0,0 +1,16 @@
+public  class PointColor extends Point {
+
+    public PointColor(int px, int py, int Red, int Green, int Blue) {
+
+    }
+
+
+
+
+
+    @Override
+    public int grayscaleDiff(Point p) {
+
+        return 0;
+    }
+}
